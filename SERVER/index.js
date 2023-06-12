941fc6247f7eeeb988e5db1a12660c1b95de7cf9
lictical <granat365@gmail.com>
2023-06-12 10:28:34 -0500

    Second Commit, Server created

--- SERVER/index.js
+++ SERVER/index.js
@@ -2,19 +2,20 @@ import express from "express"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 import http from "http"
-import mongoose from "mongoose"
-import "dotenv/config"
+import mongoose from "mongoose"                 // This allow us to connect to mongodb
+import "dotenv/config"                          // This allow us to connect to .env file
 
 const app = express();
 
-app.use(cors());
-app.use(express.json);
-app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
+app.use(cors());                                // Add cors functionality for authenticating between pages
+app.use(express.json);                          // Add express to run the server
+app.use(express.urlencoded({extended: false})); // Add url encoding
+app.use(cookieParser());                        // Add cookies parser
 
+
+/*===============  CREATE A CONNECTION TO TMONGOB AND MAKE THE PORT 5001 LISTEN  ==================================*/ 
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
-
 mongoose.connect(process.env.MONGODB_URL).then(()=>{
     console.log("Mongodb connected");
     server.listen(port,()=>{
